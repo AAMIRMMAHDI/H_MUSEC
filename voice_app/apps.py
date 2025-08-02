ed8b2504d9b0aca3c6f317369b3200d69a0bcd9d
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2025-08-02 21:21:28 +0330

    CLONE_1

--- voice_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VoiceAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "voice_app"
--- voice_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('manage/<int:user_id>/<str:action>/', views.manage_user, name='manage_user'),
-]
