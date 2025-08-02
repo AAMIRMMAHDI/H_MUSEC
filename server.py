7310a50d7d674dcafd9933d2a765c3db5d814e14
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2025-08-02 21:25:26 +0330

    CLONE_4

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from flask_socketio import SocketIO, emit
 import eventlet
-
 eventlet.monkey_patch()
 
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return '🔊 Audio relay server is running!'
+    return "Server is up!"
 
-@socketio.on('audio')
+@socketio.on("audio")
 def handle_audio(data):
-    emit('audio', data, broadcast=True, include_self=False)
+    emit("audio", data, broadcast=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
